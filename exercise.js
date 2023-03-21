3bda8d494fb3eb3e553901ba209a18b8b87fa706
mklymchuk <mklymchuk1994@gmail.com>
2023-03-21 02:36:01 +0200

    js exercise

---
+++ exercise.js
@@ -0,0 +1,12 @@
+let x = 10
+let y = 5
+
+console.log("Variables x = 10, y = 5, modulo = 13")
+console.log("Sum", x+y)
+console.log("Substraction", x-y)
+console.log("Multiply", x*y)
+console.log("Divide", x/y)
+
+let modulo = 13
+
+console.log("Modulo", 13%5)
