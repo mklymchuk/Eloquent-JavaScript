148c1d9d920371b905f57021ea278e8dc6b3153e
mklymchuk <mklymchuk1994@gmail.com>
2023-03-22 07:02:58 +0200

    sz++

---
+++ Sz++.js
@@ -0,0 +1,26 @@
+//Hello world p.1 
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-1
+
+console.log("hello world");
+
+//Hello world p.2 
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-2
+console.log("hello\nworld");
+
+//Hello world p.3 
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-3
+console.log(12892323 + 454665768);
+
+//Variables p.1
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/swap#chastina-1
+let first = 10;
+let second = 11;
+
+console.log("first = " + first);
+console.log("second = " + second + "\n");
+
+let temp1 = first;
+let temp2 = second;
+
+console.log("first = " + (first = temp2));
+console.log("second = " + (second = temp1));
