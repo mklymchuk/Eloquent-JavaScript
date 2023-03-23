27bc53db8911efe65ca6dd5d17fb80eab3ef6850
mklymchuk <mklymchuk@student.po.edu.pl>
2023-03-23 17:25:31 +0200

    sz++ ifelse

---
+++ Sz++/Sz++.js
@@ -0,0 +1,180 @@
+//Hello world p.1 
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-1
+
+console.log("hello world");
+
+//Hello world p.2 
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-2
+console.log("hello\nworld");
+
+//Hello world p.3 
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-3
+console.log(12892323 + 454665768);
+
+//Variables p.1
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/swap#chastina-1
+let first = 10;
+let second = 11;
+
+console.log("first = " + first);
+console.log("second = " + second + "\n");
+
+let temp1 = first;
+let temp2 = second;
+
+console.log("first = " + (first = temp2));
+console.log("second = " + (second = temp1));
+
+//Variables p.2
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/swap#chastina-2
+let first = 10;
+let second = 11;
+
+console.log("first = " + first);
+console.log("second = " + second + "\n");
+
+let temp1 = first;
+
+console.log("first = " + (first = second));
+console.log("second = " + (second = temp1));
+
+//Variables p.3
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/swap#chastina-3-ne-obovyazkovo
+let first = 10;
+let second = 11;
+
+console.log("first = " + first);
+console.log("second = " + second + "\n");
+
+first = first + second;
+second = first - second;
+first = first - second;
+
+console.log("first = " + first);
+console.log("second = " + second);
+
+//Next number
+// Завдання: https://shpp.gitbook.io/zero/tutorials/input-output/get-next-number
+let number = +prompt("Введіть число");
+
+number++;
+console.log(number);
+//console.log(+prompt("Введіть число") + 1);
+
+//two numbers sum
+// Завдання: https://shpp.gitbook.io/zero/tutorials/input-output/addition
+let temp1 = +prompt();
+let temp2 = +prompt();
+let sum = temp1 + temp2;
+
+console.log(temp1 + " + " + temp2 + " = " + sum);
+/*let first = +prompt("Enter the first number");
+let second = +prompt("Enter the second number");
+console.log(first + " + " + second + " = " + (first + second));*/ 
+
+//kmden
+// Завдання: https://shpp.gitbook.io/zero/tutorials/if-else/km-day-1
+let distance = +prompt("Скільки ви проходите кілометрів в день?");
+
+if (distance < 4) {
+    console.log("мало");
+} else {
+    console.log("красунчик!");
+}
+/*if (+prompt("Скільки кілометрів ти пройшов сьогодні?") <= 3) {
+    console.log("мало");
+} else {
+    console.log("красунчик!");
+}*/
+
+//kmden-2 p1
+// Завдання: https://shpp.gitbook.io/zero/tutorials/if-else/km-day-2#chastina-1
+let distance = +prompt("Скільки ви проходите кілометрів в день?");
+
+if (distance < 4) {
+    console.log("мало");
+}
+
+if (distance > 3 && distance < 20) {
+    console.log("красунчик!");
+}
+
+if (distance >= 20) {
+    console.log("марафонець!");
+}
+
+/*let distance = +prompt("Скільки кілометрів ти пройшов сьогодні?");
+
+if (distance < 4 && distance >= 0) {
+    console.log("мало");
+}
+ 
+if (distance >= 4 && distance < 20) {
+    console.log("красунчик!");
+}
+ 
+if (distance >= 20) {
+    console.log("марафонець!");
+} */
+
+//kmden-2 p2
+// Завдання: https://shpp.gitbook.io/zero/tutorials/if-else/km-day-2#chastina-2
+let distance = +prompt("Скільки ви проходите кілометрів в день?");
+
+if (distance < 20) {
+    if (distance > 3) {
+        console.log("красунчик!");
+    } else {
+        console.log("мало");
+    }
+}
+
+if (distance >= 20) {
+    console.log("марафонець!");
+}
+
+/*let distance = +prompt("Скільки кілометрів ти пройшов сьогодні?");
+ 
+if (distance >= 20) {
+    console.log("марафонець!");
+} else {
+    if (distance >= 4) {
+        console.log("красунчик!");
+    } else if (distance >= 0) {
+        console.log("мало");
+    } 
+}*/
+
+//kmden-2 p3
+// Завдання: https://shpp.gitbook.io/zero/tutorials/if-else/km-day-2#chastina-3
+let distance = +prompt("Скільки ви проходите кілометрів в день?");
+
+if (distance < 20) {
+    if (distance > 3) {
+        console.log("красунчик!");
+    }
+
+    if (distance < 4) {
+        console.log("мало");
+    }
+}
+
+if (distance >= 20) {
+    console.log("марафонець!");
+}
+/*let distance = +prompt("Скільки кілометрів ти пройшов сьогодні?");
+
+
+if (distance <= 3) {
+    console.log("мало");
+}
+
+if (distance >= 20) {
+    console.log("марафонець!");
+}
+ 
+if (distance > 3) {
+    if (distance < 20) {
+        console.log("красунчик!");
+    }
+} */
