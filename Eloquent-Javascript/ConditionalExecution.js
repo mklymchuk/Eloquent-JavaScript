df96c4a1d9fe9fb6ddba0aeaf33c37c22687e3f0
mklymchuk <mklymchuk@student.po.edu.pl>
2023-03-23 19:20:30 +0200

    if and while do while

---
+++ Eloquent-Javascript/ConditionalExecution.js
@@ -0,0 +1,16 @@
+let theNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(theNumber)){
+    console.log("Your number is the square root of " + theNumber * theNumber);
+}else{
+console.log("Hey. Why didn't you give me a number?");
+}
+
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+    console.log("Small");
+} else if (num < 100) {
+    console.log("Medium");
+} else {
+    console.log("Large")
+}
