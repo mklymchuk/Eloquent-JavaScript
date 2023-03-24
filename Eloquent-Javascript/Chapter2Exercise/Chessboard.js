1dd444401d770459477b44183b3139b336b19ebb
mklymchuk <mklymchuk@student.po.edu.pl>
2023-03-24 16:20:06 +0200

    Chessboard.js

---
+++ Eloquent-Javascript/Chapter2Exercise/Chessboard.js
@@ -0,0 +1,36 @@
+let size = +prompt("Chessboard size: ");
+let board = "";
+
+for (let triangleY = 0; triangleY <= size; triangleY++) {
+    for (let triangleX = 0; triangleX <= size; triangleX++) {
+        if ((triangleX + triangleY) % 2 == 0) {
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+
+    board += "\n";
+}
+
+console.log(board);
+
+//print() - відкриває принтер
+//рішення з книжки
+
+size = 8;
+
+board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
